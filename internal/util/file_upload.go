f02b5788d28d302d6e27354e5899e4b2f6ec7eec
Dan Munro <dan@danmunro.com>
2022-01-19 20:36:14 -0800

    logging

--- internal/util/file_upload.go
+++ internal/util/file_upload.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -9,6 +10,7 @@ import (
 func StoreUploadedFile(r *http.Request) (*os.File, error) {
 	file, _, err := r.FormFile("image")
 	if err != nil {
+		log.Print("error accessing form image :: ", err)
 		return nil, err
 	}
 	defer file.Close()
